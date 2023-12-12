95eb6793e085de706713a0d87969b9881e9ca900
mdfaysalbabu <foysalfoysal313@gmail.com>
2023-12-12 19:24:43 +0600

    global error

--- src/app/middlewear/globalErrorHandler.ts
+++ src/app/middlewear/globalErrorHandler.ts
@@ -1,18 +1,48 @@
+/* eslint-disable prefer-const */
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
+import ValidationError from '../helpError/validationError';
+import handlerCastError from '../helpError/CastError';
+import { ZodError } from 'zod';
+import handlerZodError from '../helpError/ZodError';
+import handleDuplicateError from '../helpError/duplicateError';
+import config from '../config';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
-    
   let statusCode = 500;
-  let message = 'Internal Server Error';
-  let errorMessage = err.message || 'An unexpected error occurred';
+  let errorMessage = 'Internal server error';
+  let message = err.message || 'Something went wrong';
   let errorDetails = {};
 
-  if (err instanceof AppError) {
-    statusCode = err.statusCode;
-    message = err.message;
-    errorDetails = err.details || {};
+  if (err?.name === 'ValidationError') {
+    const validationError = ValidationError(err);
+    statusCode = validationError.statusCode;
+    message = validationError.message;
+    errorMessage = validationError.errorMessage;
+    errorDetails = validationError.errorDetails;
+  } else if (err?.name === 'CastError') {
+    const castValidationError = handlerCastError(err);
+    statusCode = castValidationError.statusCode;
+    message = castValidationError.message;
+    errorMessage = castValidationError.errorMessage;
+    errorDetails = castValidationError.errorDetails;
+  } else if (err instanceof ZodError) {
+    const zodValidationError = handlerZodError(err);
+    statusCode = zodValidationError.statusCode;
+    message = zodValidationError.message;
+    errorMessage = zodValidationError.errorMessage;
+    errorDetails = zodValidationError.errorDetails;
+  } else if (err?.code === 11000) {
+    const duplicateKeyError = handleDuplicateError(err);
+    statusCode = duplicateKeyError.statusCode;
+    message = duplicateKeyError.message;
+    errorMessage = duplicateKeyError.errorMessage;
+    errorDetails = duplicateKeyError.errorDetails;
+  } else if (err instanceof Error) {
+    message = err?.message;
+    errorMessage = err?.message;
+    errorDetails = err;
   }
 
   res.status(statusCode).json({
@@ -20,7 +50,7 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     message,
     errorMessage,
     errorDetails,
-    stack: process.env.NODE_ENV === 'production' ? undefined : err.stack,
+    stack: config.NODE_ENV === 'development' ? undefined : err.stack,
   });
 };
 
